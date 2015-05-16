f1eb7c0829cb97284adc65e6005620aa651c2721
Carlos MMI <cmonzon@hotmail.es>
2015-05-16 17:01:52 +0000

    primer

---
+++ db/migrate/20150420222609_add_fields_to_users.rb
@@ -0,0 +1,8 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :date_of_birth, :datetime
+    add_column :users, :is_female, :boolean, default: false
+  end
+end
